5e25cd06755a22c3055602ea981f410d751ff520
TabNoc <meinich.spiel@gmail.com>
2022-03-18 05:48:30 +0100

    added HostBuilderExtension

---
+++ KBE.Shared/Extensions/HostBuilderExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace KBE.Shared.Extensions
+{
+	public static class HostBuilderExtension
+	{
+		public static IHostBuilder AddSecretAppSettingsJson(this IHostBuilder hostBuilder)
+		{
+			return hostBuilder.ConfigureAppConfiguration(builder =>
+			{
+				if (File.Exists("appsettings.secrets.json"))
+				{
+					builder.AddJsonFile("appsettings.secrets.json");
+					Log.Logger.Information("Applied appsettings.secrets.json");
+				}
+				else
+				{
+					Log.Logger.Information("Could not configure appsettings.secrets.json -- File not found");
+				}
+			});
+		}
+	}
+}
