28a7c442b662ae368e9c2f0cf9358dd23b17f740
TabNoc <meinich.spiel@gmail.com>
2022-03-18 05:40:17 +0100

    improved LoggingHandler

--- KBE.Shared/LoggingHelper.cs
+++ KBE.Shared/LoggingHelper.cs
@@ -11,18 +11,13 @@ namespace KBE.Shared
 	{
 		public void RegisterFullLogger(HostBuilderContext context, IServiceProvider services, LoggerConfiguration configuration)
 		{
-			LoggingConfiguration? loggingConfiguration = context.Configuration.GetUnValidatedConfiguration<LoggingConfiguration>();
-			if (loggingConfiguration != null)
-			{
-				loggingConfiguration.ValidateAndThrow();
-				if (loggingConfiguration.SeqServerUri != null)
-					configuration
-						.ReadFrom.Configuration(context.Configuration) // see https://github.com/serilog/serilog-aspnetcore for syntax
-						.ReadFrom.Services(services)
-						.Enrich.FromLogContext()
-						.WriteTo.Console()
-						.WriteTo.Seq(loggingConfiguration.SeqServerUri, apiKey: loggingConfiguration.SeqServerApiKey);
-			}
+			LoggerConfiguration loggerConfiguration = configuration
+				.ReadFrom.Configuration(context.Configuration) // see https://github.com/serilog/serilog-aspnetcore for syntax
+				.ReadFrom.Services(services)
+				.Enrich.FromLogContext()
+				.WriteTo.Console();
+
+			TrySafeRegisteringSeq(context, loggerConfiguration);
 		}
 
 		public void RegisterLoggingBootstrap()
@@ -42,6 +37,8 @@ namespace KBE.Shared
 
 				action();
 
+				Log.Information("Application terminated gracefully");
+
 				return 0;
 			}
 			catch (Exception ex)
@@ -53,6 +50,33 @@ namespace KBE.Shared
 			{
 				Log.CloseAndFlush();
 			}
+		}
+
+		private static void ApplySeqConfiguration(LoggerConfiguration loggerConfiguration, LoggingConfiguration loggingConfiguration)
+		{
+			Log.Logger.Information("Found LoggingConfiguration -> checking");
+			loggingConfiguration.ValidateAndThrow();
+
+			if (loggingConfiguration.SeqServerUri != null)
+			{
+				Log.Logger.Information("Found valid LoggingConfiguration -> registering Seq as Sink");
+				loggerConfiguration
+					.WriteTo.Seq(loggingConfiguration.SeqServerUri, apiKey: loggingConfiguration.SeqServerApiKey);
+			}
+			else
+			{
+				Log.Logger.Warning("Found valid LoggingConfiguration, but no Server was configured");
+			}
+		}
+
+		private static void TrySafeRegisteringSeq(HostBuilderContext context, LoggerConfiguration loggerConfiguration)
+		{
+			LoggingConfiguration? loggingConfiguration = context.Configuration.GetUnValidatedConfiguration<LoggingConfiguration>();
+
+			if (loggingConfiguration != null)
+				ApplySeqConfiguration(loggerConfiguration, loggingConfiguration);
+			else
+				Log.Logger.Warning("Found no LoggingConfiguration -> will not register Seq Sink");
 		}
 	}
 }
